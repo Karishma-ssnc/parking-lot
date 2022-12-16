3ebdeba39bce1e7b00dfa02515376be7b699a81d
Karishma-ssnc <120086862+Karishma-ssnc@users.noreply.github.com>
2022-12-16 11:13:40 +0530

    Added JUnit5 dependencies and dummy test cases

--- src/test/java/com/ezest/parkinglot/parkinglot/ParkingLotApplicationTests.java
+++ src/test/java/com/ezest/parkinglot/parkinglot/ParkingLotApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ezest.parkinglot.parkinglot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ParkingLotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	 @BeforeAll  
+	  public static void init() {  
+	        System.out.println("Parking Lot Testing");  
+	  }  
+	  @Test  
+	  public void testBark() {  
+	        String expectedString = "Success";  
+			assertEquals(expectedString, "Success");  
+		    System.out.println("Success!");  
+	  }  
 }
